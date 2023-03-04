541143ae2b1b08723b8dd74522d8d983bcd5bcf5
mitsuonitta <infinias320@gmail.com>
2023-03-04 13:54:57 +0900

    commit 00

---
+++ app/services/search_tweets_service.rb
@@ -0,0 +1,10 @@
+class SearchTweetsService
+  def self.search(search)
+    if search != ""
+      Tweet.where('text LIKE(?)', "%#{search}%")
+    else
+      Tweet.all
+    end
+  end
+end
+SearchTweetsServiceと
